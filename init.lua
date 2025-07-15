c479337b6d0bb0999ec2f3c578b58fe451a73352
Lachie Rouse <lachie.rouse@moretonbay.qld.gov.au>
2025-07-16 08:33:06 +1000

    Added keybinds for comma delimiting from excel columns

--- init.lua
+++ init.lua
@@ -25,6 +25,12 @@ vim.keymap.set('n', '<C-a>', 'ggVG', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', 'ys', '<Plug>(nvim-surround-normal)', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('x', 'S', '<Plug>(nvim-surround-visual)', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('o', 'S', '<Plug>(nvim-surround-operator)', { noremap = true, silent = true })
+-- Excel column to comma delimited list
+vim.api.nvim_set_keymap('n', '<C-d>', ':%s/\\r\\n/,<CR>$xyy', { noremap = true, silent = true })
+-- Excel column to single quoted comma delimited list
+vim.api.nvim_set_keymap('n', '<C-q>', ':%s/\\r\\n/\',\'<CR>$xx^i\'<Esc>yy', { noremap = true, silent = true })
+-- Handy shortcut for bridge defects to delete existing line and paste
+vim.api.nvim_set_keymap('n', '<C-p>', '\"3ddp<C-d>:%s/\\r\\n/,<CR>$xyy', { noremap = true, silent = true })
 
 --Clipboard stuff
 vim.opt.clipboard = "unnamedplus"
